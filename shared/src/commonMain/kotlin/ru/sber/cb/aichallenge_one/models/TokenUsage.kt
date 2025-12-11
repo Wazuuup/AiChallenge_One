2bf727519e8dc941b5e2b3a081797a34eb1123c9
Wazuuuuup <skostyuchenko@gmail.com>
2025-12-11 23:15:25 +0300

    refactoring

---
+++ shared/src/commonMain/kotlin/ru/sber/cb/aichallenge_one/models/TokenUsage.kt
@@ -0,0 +1,10 @@
+package ru.sber.cb.aichallenge_one.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TokenUsage(
+    val promptTokens: Int = 0,
+    val completionTokens: Int = 0,
+    val totalTokens: Int = 0
+)
